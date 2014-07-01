b434a32b3b1ef77f47a1b63295e7112507c41fbf
Bas Rustenburg <bas.rustenburg@choderalab.org>
2014-07-01 14:30:24 -0400

    Added nosetest to run script.
    Includes renaming setup script to remove dash.

---
+++ tests.py
@@ -0,0 +1,8 @@
+__author__ = 'rustenburg'
+
+def test_setup_experiment():
+    """Run the ITC preparation script."""
+    import sys
+    sys.path.append('SAMPL4-CB7/itc/')
+    import setup_experiment
+
